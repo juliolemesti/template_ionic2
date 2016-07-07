c69280113afbda5d7d00820744a96b5f1a5cac2e
Julio Lemes <juliolemesti@gmail.com>
2016-07-07 11:11:55 -0300

    Ajuste configs de teste do karma

---
+++ app/pages/page1/page1.spec.ts
@@ -0,0 +1,11 @@
+import {describe, it} from "@angular/core/testing";
+
+this.fixture = null;
+this.instance = null;
+
+describe('Page 1', () => {
+
+    it('initialises', () => {
+        console.log('initialized');
+    });
+});
